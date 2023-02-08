3adfb2c2d9e4bec3c2ee7da32a382f63c3fc1fb0
aroraarin <122515163+aroraarin@users.noreply.github.com>
2023-02-08 11:26:23 +0530

    Update Q3.sh

--- Q3.sh
+++ Q3.sh
@@ -2,26 +2,37 @@
 
 echo "Enter number to check whether it is prime or not: "
 read n
-if [ -z $n ]
-then
-    echo "No input is given. "
-else
-    i=2
-    flag=0
-    while [ $i -le `expr $n / 2` ]
-        do
-            if [ `expr $n % $i` -eq 0 ]
-            then
-                flag=1
-            fi
-        
-        i=`expr $i + 1`
-    done 
-    if [ $flag -eq 1 ]
-    then
-        echo "The number is Not Prime"
-    else
-        echo "The number is Prime" 
-    fi
-fi
 
+function Prime(){
+
+    if [ -z $n ]
+    then
+        echo "No input is given. "
+    else
+        no=$1
+
+        if [ $no -lt 2 ];then
+            echo "The number is Not Prime"
+            return
+        fi
+
+        cnt=0
+        for (( i = 2; i*i<= no ; i++ ))
+            do 
+                flag=$(( $no % $i ))
+                if [ $flag == 0 ];
+                then
+                   cnt=$((cnt+1))
+                fi
+        done
+
+        if [ $cnt == 0 ];
+         then
+            echo "The number is Prime" 
+        else
+            echo "The number is Not Prime"
+        fi
+    fi    
+}
+
+Prime $n 
