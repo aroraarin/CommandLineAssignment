8af71b2d1a7b8c2a1d98f5286a1310df42da0941
aroraarin <122515163+aroraarin@users.noreply.github.com>
2023-02-08 11:51:29 +0530

    Added comments

--- Q1.sh
+++ Q1.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-echo "Date = $(date '+%d-%m-%y')"
-echo "Current time = $(date +"%T")"
+
+#displaying the current date in DD-MM-YYYY format
+echo "Date = $(DATE '+%d-%m-%y')"
+
+#displaying the current time in HH:MM:SS format
+echo "Current time = $(DATE +"%T")"
+
+#displaying the current logged in user
 echo "Username = $USER"
+
+#displaying the home directory path
 echo "Home directory = $HOME"
+
+#displaying the current directory path
 echo "Current directory = $(pwd)"
 
