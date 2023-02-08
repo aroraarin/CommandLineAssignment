4ce5024f91f9da48b2cf5d53e699861596efb97c
aroraarin <122515163+aroraarin@users.noreply.github.com>
2023-02-08 12:03:01 +0530

    added comments

--- Q4.sh
+++ Q4.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
+
+#creating a directory named Assignment if it is not already created
 mkdir -p Assignment
+
+#traversing into Assignment directory
 cd Assignment
+
+#creating a file named File1.txt inside Assignment directory
 touch File1.txt
+
+#copying the content of Q2.sh file into File1.txt
 echo | cat ../Q2.sh > File1.txt
+
+#appending "Welcome to Sigmoid" in File1.txt
+echo "Welcome to Sigmoid" >> File1.txt
+
+#traversing to Desktop directory
 cd
+cd Desktop
+
+#listing all the contents present on desktop
 ls
 
