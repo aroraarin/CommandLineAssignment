8a0130912710b8d1fc1a19cd096263b30e7c5c57
aroraarin <122515163+aroraarin@users.noreply.github.com>
2023-02-08 12:12:47 +0530

    comments added

--- Q5.sh
+++ Q5.sh
@@ -6,17 +6,21 @@ arr=( 2 3 4 1 6 7 )
 #displaying length of array arr
 echo "Length of array is ${#arr[@]}"
 
+#initializing max
 max=$arr[0]
+#initializing min
 min=$arr[0]
 
 for i in ${arr[@]}
 do  
 
 if [[ $i > $max ]] ; then
+#updating max
 max=$i 
 fi
 
 if [[ $i < $min ]] ; then
+#updating min
 min=$i
 
 fi
