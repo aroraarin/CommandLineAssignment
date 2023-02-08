f46090b167bec1ad99fed505fe6e69bda175bf75
aroraarin <122515163+aroraarin@users.noreply.github.com>
2023-02-08 12:05:10 +0530

    comments added

--- Q5.sh
+++ Q5.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+#storing the given array in variable named arr
 arr=( 2 3 4 1 6 7 )
+
+#displaying length of array arr
 echo "Length of array is ${#arr[@]}"
+
 max=$arr[0]
 min=$arr[0]
 
@@ -18,5 +22,8 @@ min=$i
 fi
 done
 
+#displaying the maximum value present in array arr
 echo "Max value is $max "
+
+#displaying the minimum value present in array arr
 echo "Min value is $min"
