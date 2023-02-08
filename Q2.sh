fca4e509b0c8f6a02c816cd29ac7476d24641a40
aroraarin <122515163+aroraarin@users.noreply.github.com>
2023-02-08 11:53:22 +0530

    Comments added

--- Q2.sh
+++ Q2.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 echo "Enter number for which table is to be generated: "
+#taking input from the user
 read n
+
+#checking whether an input is given or not
 if [ -z $n ]
 then
     echo "No input is given. "
 else
     a=1
+    #loop to create the table for the given input
    while [ $a -lt 11 ]
    do  
        ans=`expr $n \* $a`
